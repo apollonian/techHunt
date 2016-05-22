9a0888c3268f14503995d26676adfec2900728b9
Abhishek Warokar <abhishek11@live.in>
2016-05-23 00:28:40 +0530

    added yesterdayPosts & utterances

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 'use strict';
-/**
-    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
-    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-
-        http://aws.amazon.com/apache2.0/
-
-    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-*/
 
 /**
- * One-shot model:
+ *  Daily Hunt
+ * 
+ * 
+ *  One-shot model:
  *  User: "Alexa, what's trending on Product Hunt?"
  *  Alexa: "Here are the top two products: ..."
  */
@@ -19,11 +13,6 @@
  * App ID for the skill
  */
 var APP_ID = undefined; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
-
-/**
- * Top Hunts.
- */
-var top = "ABC, a description of ABC. And, DEF, a description of DEF. ";
 
 /**
  * The AlexaSkill prototype and helper functions
@@ -36,111 +25,107 @@ var AlexaSkill = require('./AlexaSkill');
  *
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
  */
-var dailyHunt = function () {
+var dailyHunt = function() {
     AlexaSkill.call(this, APP_ID);
 };
 
-/** 
- * Load the Product Hunt wrapper
- * https://github.com/danillouz/product-hunt
- */
-const productHunt = require('product-hunt');
-
-/**
- * Retrieve today's popular products 
- */ 
-const getProductsPromiseToday = productHunt.today().popular().exec()
-
-//TODO: Speak top 2 products or top 3?
-
-var topProducts=[], today;
-getProductsPromiseToday
-	.then(function productsFetcher(products) {
-		if(products==null){
-		    today=0;
-        	return;
-        }
-		var count=0, i=0;
-		while(count!=3){
-			if(products[i].category_id==1){
-				topProducts[i]=[products[i].name, products[i].tagline];
-				count++;
-			}
-			i++;
-		}
-	});
-
-/**
- * Retrieve yesterday's popular products 
- */
-if(!today){
-    const getProductsPromise_yesterday = yesterday().popular().exec();
-    getProductsPromise_yesterday
-        .then(function productsFetcher(products) {
-        var count=0, i=0;
-		while(count!=3){
-			if(products[i].category_id==1){
-				topProducts[i]=[products[i].name, products[i].tagline];
-				count++;
-			}
-			i++;
-		}
-    });
- }
 // Extend AlexaSkill
 dailyHunt.prototype = Object.create(AlexaSkill.prototype);
 dailyHunt.prototype.constructor = dailyHunt;
 
-dailyHunt.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
-    console.log("dailyHunt onSessionStarted requestId: " + sessionStartedRequest.requestId
-        + ", sessionId: " + session.sessionId);
+dailyHunt.prototype.eventHandlers.onSessionStarted = function(sessionStartedRequest, session) {
+    console.log("dailyHunt onSessionStarted requestId: " + sessionStartedRequest.requestId + ", sessionId: " + session.sessionId);
     // any initialization logic goes here
 };
 
-dailyHunt.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+dailyHunt.prototype.eventHandlers.onLaunch = function(launchRequest, session, response) {
     console.log("dailyHunt onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
     handleGetTopRequest(response);
 };
 
-dailyHunt.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
-    console.log("dailyHunt onSessionEnded requestId: " + sessionEndedRequest.requestId
-        + ", sessionId: " + session.sessionId);
+dailyHunt.prototype.eventHandlers.onSessionEnded = function(sessionEndedRequest, session) {
+    console.log("dailyHunt onSessionEnded requestId: " + sessionEndedRequest.requestId + ", sessionId: " + session.sessionId);
     // any cleanup logic goes here
 };
 
 dailyHunt.prototype.intentHandlers = {
-    "GetTopProductsIntent": function (intent, session, response) {
+    "GetTopProductsIntent": function(intent, session, response) {
         handleGetTopRequest(response);
     },
 
-    "AMAZON.HelpIntent": function (intent, session, response) {
+    "AMAZON.HelpIntent": function(intent, session, response) {
         response.ask("You can ask Daily Hunt what's trending on Product Hunt, or, you can say exit... What can I help you with?", "What can I help you with?");
     },
 
-    "AMAZON.StopIntent": function (intent, session, response) {
+    "AMAZON.StopIntent": function(intent, session, response) {
         var speechOutput = "Goodbye";
         response.tell(speechOutput);
     },
 
-    "AMAZON.CancelIntent": function (intent, session, response) {
+    "AMAZON.CancelIntent": function(intent, session, response) {
         var speechOutput = "Goodbye";
         response.tell(speechOutput);
     }
 };
 
 /**
- * Get the 'top' string and return to the user.
+ * Get top products return to the user.
  */
 function handleGetTopRequest(response) {
     // Create speech output
-    var speechOutput = "Here are the top two products: " + top;
-    response.tellWithCard(speechOutput, "dailyHunt", speechOutput);
+    /** 
+     * Load the Product Hunt wrapper
+     * https://github.com/danillouz/product-hunt
+     */
+    const productHunt = require('product-hunt');
+
+    /**
+     * Retrieve today's popular products 
+     */
+    const getProductsPromiseToday = productHunt.today().popular().exec();
+    var today = 1;
+    getProductsPromiseToday
+        .then(function productsFetcher(products) {
+            if (products == null) {
+                today = 0;
+                return;
+            }
+            var count = 0,
+                i = 0,
+                topProducts = [];
+            while (count != 3) {
+                if (products[i].category_id == 1) {
+                    topProducts[i] = [products[i].name, products[i].tagline];
+                    count++;
+                }
+                i++;
+            }
+            var speechOutput = "Here are the top two products: " + topProducts[0][0] + topProducts[0][1] + " and " + topProducts[1][0] + topProducts[1][1];
+            response.tellWithCard(speechOutput, "dailyHunt", speechOutput);
+        });
+    if (!today) {
+        const getProductsPromiseYesterday = productHunt.yesterday().popular().exec();
+        getProductsPromiseYesterday
+            .then(function productsFetcher(products) {
+                var count = 0,
+                    i = 0,
+                    topProducts = [];
+                while (count != 3) {
+                    if (products[i].category_id == 1) {
+                        topProducts[i] = [products[i].name, products[i].tagline];
+                        count++;
+                    }
+                    i++;
+                }
+                var speechOutput = "Here are the top two products: " + topProducts[0][0] + topProducts[0][1] + " and " + topProducts[1][0] + topProducts[1][1];
+                response.tellWithCard(speechOutput, "dailyHunt", speechOutput);
+            });
+    }
 }
 
 // Create the handler that responds to the Alexa Request.
-exports.handler = function (event, context) {
+exports.handler = function(event, context) {
     // Create an instance of the dailyHunt skill.
     var dailyHunt = new dailyHunt();
     dailyHunt.execute(event, context);
 };
-
